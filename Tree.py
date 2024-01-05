122a081aa160b86ed35996e2f0ce0eae8518e918
Jonatan Kosower <jonatankoso@gmail.com>
2024-01-05 15:27:34 +0200

    fixed bugs of cases of operations

--- Tree.py
+++ Tree.py
@@ -16,34 +16,42 @@ def evaluate(node):
         return int(node.value)
 
     # else evaluate the left and right subtrees
-    left_value = evaluate(node.left)
-    right_value = evaluate(node.right)
+    if node.left:
+        left_value = evaluate(node.left)
+    if node.right:
+        right_value = evaluate(node.right)
 
     # Perform operation based on the operator this node represents
-    if node.value == '+':
-        return left_value + right_value
-    elif node.value == '-':
-        return left_value - right_value
-    elif node.value == '*':
-        return left_value * right_value
-    elif node.value == '/':
-        return left_value // right_value
-    elif node.value == '^':
-        return right_value ** left_value
-    elif node.value == '@':
-        return float(right_value + left_value) / 2.0
-    elif node.value == '$':
-        return max(right_value, left_value)
-    elif node.value == '&':
-        return min(right_value, left_value)
-    elif node.value == '%':
-        return right_value % left_value
-    elif node.value == '~':
-        return right_value * -1
-    elif node.value == '!':
-        result = 1
-        for idx in range(1, left_value + 1):
-            result *= idx
-        return result
-    else:
-        raise ValueError("Unknown operator")
+
+    if node.right and not node.left:
+        if node.value == '~':
+            return right_value * -1
+
+    if node.left and not node.right:
+        if node.value == '!':
+            result = 1
+            for idx in range(1, left_value + 1):
+                result *= idx
+            return result
+
+    if node.left and node.right:
+        if node.value == '+':
+            return left_value + right_value
+        elif node.value == '-':
+            return left_value - right_value
+        elif node.value == '*':
+            return left_value * right_value
+        elif node.value == '/':
+            return left_value // right_value
+        elif node.value == '^':
+            return right_value ** left_value
+        elif node.value == '@':
+            return float(right_value + left_value) / 2.0
+        elif node.value == '$':
+            return max(right_value, left_value)
+        elif node.value == '&':
+            return min(right_value, left_value)
+        elif node.value == '%':
+            return right_value % left_value
+        else:
+            raise ValueError("Wrong sons to operator")
