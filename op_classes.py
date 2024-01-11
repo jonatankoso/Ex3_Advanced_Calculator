8ea62ca2bb0d99310c2978a6ef41de3b6a5a7177
Jonatan Kosower <jonatankoso@gmail.com>
2024-01-11 11:24:34 +0200

    added ^ and @ classes

--- op_classes.py
+++ op_classes.py
@@ -64,3 +64,41 @@ class DivOp:
             raise ValueError("Cannot divide by 0: /")
 
         return True
+
+class PowOp:
+    def __init__(self, isBinary, canComeBefore, canComeAfter):
+        isBinary = True
+        canComeBefore = type(str)
+        canComeAfter = type(str)
+
+    def action(self, token1, token2):
+        return float(token1) ** float(token2)
+
+    def possibleErrors(self, token1, token2):
+        if not str(token1).isnumeric() or str(token1).isnumeric():
+            raise TypeError("Tokens need to be numeric: ^")
+        if float(token1) > MAX_FLOAT or float(token2) > MAX_FLOAT or float(token1) ** float(token2) > MAX_FLOAT:
+            raise ValueError("Numbers or their result is too big: ^")
+        if isinstance(token1, Operator) or isinstance(token2, Operator):
+            raise TypeError("action only works on non-operators: ^")
+
+        return True
+
+class AvgOp:
+    def __init__(self, isBinary, canComeBefore, canComeAfter):
+        isBinary = True
+        canComeBefore = type(str)
+        canComeAfter = type(str)
+
+    def action(self, token1, token2):
+        return (float(token1) + float(token2)) / 2.0
+
+    def possibleErrors(self, token1, token2):
+        if not str(token1).isnumeric() or str(token1).isnumeric():
+            raise TypeError("Tokens need to be numeric: @")
+        if float(token1) > MAX_FLOAT or float(token2) > MAX_FLOAT:
+            raise ValueError("Numbers or their result is too big: @")
+        if isinstance(token1, Operator) or isinstance(token2, Operator):
+            raise TypeError("action only works on non-operators: @")
+
+        return True
