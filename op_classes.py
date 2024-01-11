07fc637266f70fa444ad15b0c6c50516f1db95c2
Jonatan Kosower <jonatankoso@gmail.com>
2024-01-11 11:33:24 +0200

    added $ and & classes

--- op_classes.py
+++ op_classes.py
@@ -102,3 +102,41 @@ class AvgOp:
             raise TypeError("action only works on non-operators: @")
 
         return True
+
+class MaxOp:
+    def __init__(self, isBinary, canComeBefore, canComeAfter):
+        isBinary = True
+        canComeBefore = type(str)
+        canComeAfter = type(str)
+
+    def action(self, token1, token2):
+        return max(float(token1), float(token2))
+
+    def possibleErrors(self, token1, token2):
+        if not str(token1).isnumeric() or str(token1).isnumeric():
+            raise TypeError("Tokens need to be numeric: $")
+        if float(token1) > MAX_FLOAT or float(token2) > MAX_FLOAT:
+            raise ValueError("Numbers or their result is too big: $")
+        if isinstance(token1, Operator) or isinstance(token2, Operator):
+            raise TypeError("action only works on non-operators: $")
+
+        return True
+
+class MinOp:
+    def __init__(self, isBinary, canComeBefore, canComeAfter):
+        isBinary = True
+        canComeBefore = type(str)
+        canComeAfter = type(str)
+
+    def action(self, token1, token2):
+        return min(float(token1), float(token2))
+
+    def possibleErrors(self, token1, token2):
+        if not str(token1).isnumeric() or str(token1).isnumeric():
+            raise TypeError("Tokens need to be numeric: &")
+        if float(token1) > MAX_FLOAT or float(token2) > MAX_FLOAT:
+            raise ValueError("Numbers or their result is too big: &")
+        if isinstance(token1, Operator) or isinstance(token2, Operator):
+            raise TypeError("action only works on non-operators: &")
+
+        return True
