8afc8607b1846859b518d9349c8bcf354404eb26
Jonatan Kosower <jonatankoso@gmail.com>
2024-01-11 11:18:58 +0200

    added / and * classes

--- op_classes.py
+++ op_classes.py
@@ -1,3 +1,4 @@
+MAX_FLOAT = 1.7976931348623157e+308
 class Operator:
   def __init__(self, isBinary, canComeBefore):
     self.isBinary = True
@@ -8,18 +9,58 @@ class Operator:
 class AddOp:
     def __init__(self, isBinary, canComeBefore, canComeAfter):
         isBinary = True
-        canComeBefore = type(int)
-        canComeAfter = type(int)
+        canComeBefore = type(str)
+        canComeAfter = type(str)
 
     def action(self, token1, token2):
-        return int(token1) + int(token2)
+        return float(token1) + float(token2)
 
     def possibleErrors(self, token1, token2):
         if not str(token1).isnumeric() or str(token1).isnumeric():
             raise TypeError("Tokens need to be numeric: +")
-        if int(token1) > 4294967296 or int(token2) > 4294967296:
+        if float(token1) > MAX_FLOAT or float(token2) > MAX_FLOAT:
             raise ValueError("Number too big for operation: +")
         if isinstance(token1, Operator) or isinstance(token2, Operator):
             raise TypeError("action only works on non-operators: +")
 
-        return 1
+        return True
+
+class MulOp:
+    def __init__(self, isBinary, canComeBefore, canComeAfter):
+        isBinary = True
+        canComeBefore = type(str)
+        canComeAfter = type(str)
+
+    def action(self, token1, token2):
+        return float(token1) * float(token2)
+
+    def possibleErrors(self, token1, token2):
+        if not str(token1).isnumeric() or str(token1).isnumeric():
+            raise TypeError("Tokens need to be numeric: *")
+        if float(token1) > MAX_FLOAT or float(token2) > MAX_FLOAT:
+            raise ValueError("Number too big for operation: *")
+        if isinstance(token1, Operator) or isinstance(token2, Operator):
+            raise TypeError("action only works on non-operators: *")
+
+        return True
+
+class DivOp:
+    def __init__(self, isBinary, canComeBefore, canComeAfter):
+        isBinary = True
+        canComeBefore = type(str)
+        canComeAfter = type(str)
+
+    def action(self, token1, token2):
+        return float(token1) / float(token2)
+
+    def possibleErrors(self, token1, token2):
+        if not str(token1).isnumeric() or str(token1).isnumeric():
+            raise TypeError("Tokens need to be numeric: /")
+        if float(token1) > MAX_FLOAT or float(token2) > MAX_FLOAT:
+            raise ValueError("Number too big for operation: /")
+        if isinstance(token1, Operator) or isinstance(token2, Operator):
+            raise TypeError("action only works on non-operators: /")
+        if float(token2) == 0:
+            raise ValueError("Cannot divide by 0: /")
+
+        return True
