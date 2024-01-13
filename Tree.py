21f3751853089241117968a5d5e2c4a995fefae3
Jonatan Kosower <jonatankoso@gmail.com>
2024-01-13 11:20:06 +0200

    Added comments, added unary expressions to generalErrors

--- Tree.py
+++ Tree.py
@@ -1,18 +1,27 @@
 class node:
     def __init__(self, value):
+        '''
+        Function builds a tree node based on given value
+        :param value: data of node
+        '''
         self.left = None
         self.value = value
         self.right = None
 
     def clone(self):
+        '''
+        Function clones a tree node
+        :return: clone of given node
+        '''
         n = node(self.value)
         n.left = None if self.left is None else self.left.clone()
         n.right = None if self.right is None else self.right.clone()
         return n
 
+
 def evaluate(node):
     '''
-    function receives a node of the syntax tree and recursively evaluates it
+    function receives a node of the expression tree and recursively evaluates it
     :return: result of evaluation
     '''
     # if the value is a digit, return it as an integer
@@ -20,28 +29,12 @@ def evaluate(node):
         return float(node.value)
 
     # else evaluate the left and right subtrees
-
     if node.left:
         left_value = evaluate(node.left)
     if node.right:
         right_value = evaluate(node.right)
 
     # Perform operation based on the operator this node represents
-
-    # if node.right and not node.left:
-    #    if node.value == '~':
-    #        return right_value * -1
-
-    # if node.left and not node.right:
-    #    if node.value == '!':
-    #        if left_value >= 0:
-    #            result = 1
-    #            for idx in range(1, left_value + 1):
-    #                result *= idx
-    #            return result
-    #        else:
-    #            raise ValueError("! is an operation on natural numbers")
-
     if node.left and node.right:
         if node.value == '+':
             return left_value + right_value
@@ -106,61 +99,75 @@ def constructTree(postfix):
 
 
 def constructTreeFromInfix(infix):
+    '''
+    Function constructs an expression tree from given equation
+    :param infix: str representation of the equation
+    :return: expression tree representing the equation
+    '''
+
+    # Setting precedence of operators
     prec = {'(': 1, '+': 2, '-': 2, '*': 3, '/': 3, '^': 4, '%': 5, '@': 6, '$': 6, '&': 6, '!': 7, '~': 7, '#': 7}
     op_queue = []
     node_stack = []
     tokenList = strToList(infix)
-    print(tokenList)
+    print(tokenList)    # Check for mistakes in converting expression to list
     open_paren_count = 0
     last_unary_prefix_seen = None
 
+    # check for each one of 4 options for token, '(', number, operator, ')'
     for token in tokenList:
+        # check for 1st option, if token is (
         if token == '(':
             op_queue.insert(0, token)
 
+            # Check for parenthesis after ~ (what expression to negate)
             if last_unary_prefix_seen is not None:
                 open_paren_count += 1
+
+        # check for 2nd option, if token is number
         elif token.isdigit():
             node_stack.insert(0, node(token))
 
+            # if ~ (or any other prefix unary operator) comes before the current expression (number in this case)
             if last_unary_prefix_seen is not None and open_paren_count == 0:
                 # code dup
-                while len(op_queue) > 0 and prec[op_queue[0]] >= prec[last_unary_prefix_seen]:
-                    combine(op_queue, node_stack)
+                last_unary_prefix_seen = insertCloned(op_queue, node_stack, prec, last_unary_prefix_seen)
 
-                node_stack.insert(0, node_stack[0].clone())
-                op_queue.insert(0, last_unary_prefix_seen)
-                last_unary_prefix_seen = None
-
+        # check for 3rd option, ')'
         elif token == ')':
+            # combine the operator and operands into one expression
             while not op_queue[0] == '(':
                 combine(op_queue, node_stack)
             del op_queue[0]
 
+            # if any unary operator came before the (, make sure to reduce the number of open parenthesis after it
             if last_unary_prefix_seen is not None:
                 open_paren_count -= 1
 
+                # if the unary operation needs to be executed on the closed expression
                 if open_paren_count == 0:
                     # code dup
-                    while len(op_queue) > 0 and prec[op_queue[0]] >= prec[last_unary_prefix_seen]:
-                        combine(op_queue, node_stack)
+                    last_unary_prefix_seen = insertCloned(op_queue, node_stack, prec, last_unary_prefix_seen)
 
-                    node_stack.insert(0, node_stack[0].clone())
-                    op_queue.insert(0, last_unary_prefix_seen)
-                    last_unary_prefix_seen = None
+        # check for 4th option, operator
         else:
+            # if a new operator with lower precedence is inserted, combine the previous expression
             while len(op_queue) > 0 and prec[op_queue[0]] >= prec[token]:
                 combine(op_queue, node_stack)
 
+            # special case for prefix unary expressions
             if token == '~':
                 last_unary_prefix_seen = token
                 open_paren_count = 0
+            # special case for postfix unary expressions
             else:
                 if token == '!' or token == '#':
                     node_stack.insert(0, node_stack[0].clone())
 
+                # case for normal operators
                 op_queue.insert(0, token)
 
+    # if any last expression left
     while len(node_stack) > 1:
         combine(op_queue, node_stack)
 
@@ -168,6 +175,12 @@ def constructTreeFromInfix(infix):
 
 
 def combine(operators, nodes):
+    '''
+    Functions merges the operator and it's operands into an expression tree
+    :param operators: operators stack (contains recent operators)
+    :param nodes: nodes stack (contains recent operands)
+    :return: node with the operator as root and the operands as leaves
+    '''
     root = node(operators[0])
     del operators[0]
     root.right = nodes[0]
@@ -178,23 +191,37 @@ def combine(operators, nodes):
 
 
 def expresssionErrors(infix):
+    '''
+    Function finds general mistakes in the expression itself
+    :param infix: string representation of the expression
+    :return: none
+    '''
     if len(infix) < 1:
         raise ValueError("Empty expression is prohibited")
 
     token_list = strToList(infix)
     for token in range(len(token_list) - 1):
-        if (not token_list[token].isdigit()) and (not token_list[token] in "()+*&^%$@/"):
+        if (not token_list[token].isdigit()) and (not token_list[token] in "()+*&^%$@/!~#"):
             raise ValueError("only numbers and operators allowed")
 
-        if token_list[token] in "(+*&^%$@/" and token_list[token + 1] in "+*&^%$@/":
-            raise ValueError("No Consecutive operators allowed")
+        if token_list[token] in "(+*&^%$@/~" and token_list[token + 1] in "+*&^%$@/":
+            raise ValueError(
+                "No Consecutive operators of such kind allowed: " + token_list[token] + " " + token_list[token + 1])
 
     for token in range(len(token_list)):
         if token_list[token] in "+*&^%$@/" and (token == 0 or token == len(token_list) - 1):
             raise ValueError("wrong use of binary operators")
 
+        if (token_list[token] == '~' and token == len(token_list) - 1) or (token_list[token] in '!#' and token == 0):
+            raise ValueError("wrong use of unary operators")
+
 
 def strToList(infix):
+    '''
+    Function converts a string into a list based on operators
+    :param infix: string representation of the expression
+    :return: list representation of the expression conjoined by operators
+    '''
     divided = []
     adder = ""
     for char in infix:
@@ -209,14 +236,28 @@ def strToList(infix):
         divided.append(adder)
     return divided
 
+def insertCloned(operators, nodes, prec_map, last_unary):
+    while len(operators) > 0 and prec_map[operators[0]] >= prec_map[last_unary]:
+        combine(operators, nodes)
+
+    nodes.insert(0, nodes[0].clone())
+    operators.insert(0, last_unary)
+    return None
+
 def sum_of_digits(num):
+    '''
+    Function calculates the sum of digits of a given number
+    :param num: number to calculate the sum of
+    :return: sum of the number's digits
+    '''
     sum = 0
     while num != 0:
-        sum += int(num%10)
+        sum += int(num % 10)
         num /= 10
     return sum
 
-exp = "23##"
-#expresssionErrors(exp)
+
+exp = "~(5*2+~(3!))"
+expresssionErrors(exp)
 ans = constructTreeFromInfix(exp)
 print(evaluate(ans))
